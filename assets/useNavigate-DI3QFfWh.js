7f33cf487b4af3ac38ae5021513eb0b687f3457b
PNWIO <327253958+PNWIO@users.noreply.github.com>
2026-09-21 04:15:54 +0000

    Publish Parkview at Hostmark.

---
+++ assets/useNavigate-DI3QFfWh.js
@@ -0,0 +1,1 @@
+import{U as e,V as t,f as n}from"./button-CECqRK2l.js";var r=e(t(),1);function i(e){let t=n();return r.useCallback(n=>t.navigate({...n,from:n.from??e?.from}),[e?.from,t])}export{i as t};
